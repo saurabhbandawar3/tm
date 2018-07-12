b3327993b3d7cb0c1228f362a0903c948a43446a
Saurabh Bandawar <saurabhbandawar3@gmail.com>
2018-07-11 22:34:13 -0400

    get route added for users

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const app = express();
 const cors = require('cors');
 const _ = require('lodash');
 
-var {mongoose} = require('./db/mongoose');
-var {User} = require('./models/user')
+var {
+    mongoose
+} = require('./db/mongoose');
+var {
+    User
+} = require('./models/user')
 
-const {MongoClient, ObjectID} = require('mongodb');
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
 var obj = new ObjectID();
 
 app.use(cors());
-app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.json({
+    extended: true
+}));
 
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['fname', 'lname', 'email',
@@ -28,21 +37,36 @@ app.post('/users', (req, res) => {
 });
 
 app.post('/tasks', (req, res) => {
-    var task = _.pick(req.body, ['ttitle','aName','aEmail','eName','eEmail',
-            'deadline','tDetails']);
+    var task = _.pick(req.body, ['ttitle', 'aName', 'aEmail', 'eName', 'eEmail',
+        'deadline', 'tDetails'
+    ]);
     console.log(task);
-    
-    MongoClient.connect('mongodb://localhost:27017/taskdb', {useNewUrlParser: false}, (err, client, ) => {
-        if (err) {
-            return console.log('Unable to connect to MongoDB server');
-        }
-        const db = client.db('taskdb');
-        db.collection('users').findOneAndUpdate({email:req.body.eEmail}, {$set: {tasks: task}}, {returnOrignil: true})
-        .then((result)=> {console.log(result)});
-    });
-
+    var UsersM = mongoose.model('users', User);
+    UsersM.findOneAndUpdate({
+            email: req.body.eEmail
+        }, {
+            $set: {
+                tasks: task
+            }
+        }, {
+            returnOrignil: true
+        })
+        .then((result) => {
+            console.log(result)
+        });
 });
-
+app.get('/users', (req, res) => {
+    var UsersM = mongoose.model('users', User);
+    UsersM.find({}).then((usresData) => {
+        console.log(usresData);
+        res.send({
+            usresData
+        });
+    }, (e) => {
+        //console.log(e);
+        res.status(400).send(e);
+    });
+});
 
 
 app.post('/login', (req, res) => {
@@ -58,39 +82,6 @@ app.listen(3000, () => {
     console.log('Server listening to port 3000');
 });
 
-
-
-
-
-
-
-
-
-// MongoClient.connect('mongodb://localhost:27017/taskdb', {useNewUrlParser: true}, (err, client) => {
-//     if (err) {
-//         return console.log('Unable to connect to MongoDB server');
-//     }
-//     const db = client.db('taskdb');
-//     db.collection('Users').insertOne(body, (err, result) => {
-//         if (err) {
-//             res.send({'error': 'Unable to insert user'});
-//         }
-//         console.log(result.ops[0]._id.getTimestamp());
-//         res.send(result.ops[0]);
-//     });
-
-// });
-
-// MongoClient.connect('mongodb://localhost:27017/taskdb',{useNewUrlParser: true}, (err, client) => {
-    //     if (err) {
-    //         return console.log('Unable to connect to MongoDB server');
-    //     }
-    //     const db = client.db('taskdb');
-    //     db.collection('Tasks').insert(task, (err, result) => {
-    //         if (err) {
-    //             res.send({'error': 'Unable to insert task'});
-    //         }
-    //         console.log(JSON.stringify(result.ops[0]._id    , undefined, 2));
-    //         res.send(result.ops[0]._id);
-    //     });
-    // });
+module.exports = {
+    app
+}
