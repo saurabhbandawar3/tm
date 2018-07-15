6fc59f04ddd7a0420acdf3f1af66eeef2c4c44f9
Saurabh Bandawar <saurabhbandawar3@gmail.com>
2018-07-15 04:25:38 -0400

    user login function added

--- app.js
+++ app.js
@@ -5,27 +5,13 @@ const cors = require('cors');
 const _ = require('lodash');
 
 
-var {
-    mongoose
-} = require('./db/mongoose');
-var {
-    User
-} = require('./models/user')
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user')
 
-var {
-    authenticate
-} = require('./middleware/authenticate')
-
-const {
-    MongoClient,
-    ObjectID
-} = require('mongodb');
-var obj = new ObjectID();
+var {authenticate} = require('./middleware/authenticate')
 
 app.use(cors());
-app.use(bodyParser.json({
-    extended: true
-}));
+app.use(bodyParser.json({extended: true}));
 
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['fname', 'lname', 'email',
@@ -43,40 +29,31 @@ app.post('/users', (req, res) => {
     });
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/users/tasks', (req, res) => {
     var task = _.pick(req.body, ['ttitle', 'aName', 'aEmail', 'eName', 'eEmail',
         'deadline', 'tDetails'
     ]);
     console.log(task);
-    User.findOneAndUpdate({
-            email: req.body.eEmail
-        }, {
-            $push: {
-                tasks: task
-            }
-        }, {
+    User.findOneAndUpdate({email: req.body.eEmail}, {$push: {tasks: task}}, {
             returnOrignil: true
         })
         .then((result) => {
-            console.log(result)
+            console.log(result);
+            res.send(result);
         });
 });
 
 app.get('/users', (req, res) => {
     User.find({}).then((usresData) => {
-        console.log(usresData);
-        res.send({
-            usresData
-        });
+        res.send({usresData});
     }, (e) => {
         //console.log(e);
         res.status(400).send(e);
     });
 });
 
-app.post('/login', (req, res) => {
+app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
-    console.log(body);
     User.findByCredentials(body.email, body.password).then((user) => {
         return user.generateAuthToken().then((token) => {
             res.header('x-auth', token).send(user);
@@ -85,7 +62,6 @@ app.post('/login', (req, res) => {
         res.status(400).send();
     });
 });
-
 
 app.get('/users/me', authenticate, (req, res) => {
     console.log('res:',req.user);
--- models/user.js
+++ models/user.js
@@ -152,7 +152,23 @@ UserSchema.pre('save', function (next) {
         next();
     }
 });
-
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
 var User = mongoose.model('users', UserSchema);
 
 
